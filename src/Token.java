1622a3d6065fce82988ed4cbc69bd47adc512ae1
SaidT-dev <said.tadjine@se.univ-bejaia.dz>
2025-11-22 22:06:53 +0100

    Creation des structures Token

---
+++ src/Token.java
@@ -0,0 +1,16 @@
+public class Token {
+    public TokenType type;
+    public String val;
+    public int ligne;
+
+    public Token(TokenType type, String val, int ligne) {
+        this.type = type;
+        this.val = val;
+        this.ligne = ligne;
+    }
+
+    @Override
+    public String toString() {
+        return "Token { type: " + type +", val: \"" + val + "\", ligne:" + ligne + " }";
+    }
+}
